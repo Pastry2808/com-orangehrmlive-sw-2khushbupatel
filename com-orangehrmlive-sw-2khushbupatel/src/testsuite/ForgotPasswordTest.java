c677c1cfe5c76c73e7ca58c6594cd30b8b990783
Pastry2802 <khushbu2827@gmail.com>
2023-11-30 20:31:18 +0000

    updated

---
+++ com-orangehrmlive-sw-2khushbupatel/src/testsuite/ForgotPasswordTest.java
@@ -0,0 +1,32 @@
+package testsuite;
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ForgotPasswordTest extends BaseTest {
+    String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+
+    @Before
+    public void setUp(){
+        openBrowser(baseUrl);
+    }
+    @Test
+    public void userShouldNavigateToForgotPasswordPageSuccessfully(){
+//        *click on the ‘Forgot your password’ link
+        WebElement forgotPassword = driver.findElement(By.xpath("//div[@class='orangehrm-login-forgot']/p"));
+        forgotPassword.click();
+//        * Verify the text ‘Reset Password’
+//        String actualResult = driver.findElement(By.xpath("//div[@id='app']/div[1]/div[1]/div/form/h6")).getText();
+//        System.out.println(actualResult);
+//        String expectedResult = "Reset Password";
+//        Assert.assertEquals(actualResult,expectedResult);
+    }
+    @After
+    public void endTest(){
+        //closeBrowser();
+    }
+}
